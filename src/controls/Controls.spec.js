022175378b01022e55c89fe3464c2bfaeaefb6f7
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-08-19 18:27:16 -0500

    the tests in controlspec should work, but they arnt

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -12,9 +12,50 @@ describe('<Controls />', () => {
 
     expect(tree).toMatchSnapshot();
   });
-    it('button should lock when clicked', () => {
-    const { getByText } = render(<Controls locked={ true } closed={ false }/>);
-    expect(getByText(/lock gate/i));
-    expect(getByText(/close gate/i));
+    it('Opened Gate', () => {
+        const { getByText } = render(<Controls/>);
+            expect(getByText(/lock gate/i));
+            expect(getByText(/close gate/i));
+     })
+    it('Closed Gate', () => {
+        const { getByText } = render(<Controls/>);
+         fireEvent.click(getByText(/Close Gate/i),);
+            expect(getByText(/Lock Gate/i));
+            expect(getByText(/Close Gate/i));
+     })
+    // it('Closed Gate', () => {
+    //     const { getByText } = render(<Controls/>);
+    //       getByText('Close Gate'),
+    //         new MouseEvent('click', {
+    //             locked: false,
+    //             closed: true
+    //         })
+    //         )
+    //      fireEvent.click(getByText(/Close Gate/i),);
+    //         expect(getByText(/Lock Gate/i));
+    //         expect(getByText(/Close Gate/i));
+    //  })
+    it('Lock gate', () => {
+        const { getByText } = render(<Controls/>);
+            fireEvent.click(getByText(/close gate/i));
+             Controls.props.closed = true;
+                expect(getByText(/unlock gate/i));
+                expect(getByText(/open gate/i));
+    });
+    it('Unlock gate', () => {
+        const { getByText } = render(<Controls/>);
+        Controls.props.closed = true;
+        Controls.props.locked = true;
+            fireEvent.click(getByText(/unlock gate/i));
+                expect(getByText(/lock gate/i));
+                expect(getByText(/open gate/i));
+    });
+    it('Open gate', () => {
+        const { getByText } = render(<Controls/>);
+        Controls.props.locked = false;
+        Controls.props.closed = true;
+            fireEvent.click(getByText(/open gate/i));
+                expect(getByText(/close gate/i));
+                expect(getByText(/lock gate/i));
   });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -9,7 +9,6 @@ import Display from './Display';
 describe('<Controls />', () => {
   it('should match snapshot', () => {
     const tree = renderer.create(<Display />).toJSON();
-
     expect(tree).toMatchSnapshot();
   });
 });
