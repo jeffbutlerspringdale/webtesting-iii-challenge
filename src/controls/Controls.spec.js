b82ed6ac357fd0e0dd96440da2394da2756e62eb
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-08-19 16:56:23 -0500

    first test working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+
+import React from 'react'
+
+import renderer from 'react-test-renderer';
+import render from 'react-test-library';
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Controls />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('displays defaults as unlocked and closed', () => {
+      const tree = render(<Controls />);
+  })
+});
